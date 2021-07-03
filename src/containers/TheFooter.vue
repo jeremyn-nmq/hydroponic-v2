a6c3a7c3be143923e0cb7509be2aec334fa3d428
Quân Nguyễn Minh <nmq@netcompany.com>
2021-07-03 14:34:33 +0700

    init

---
+++ src/containers/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <CFooter :fixed="false">
+    <div>
+      <a href="https://coreui.io" target="_blank">Luong Nguyen The Minh</a>
+    </div>
+    <div class="mfs-auto">
+      <span class="mr-1" target="_blank">Project: </span>
+      <a href="https://coreui.io/vue">Hydroponic</a>
+    </div>
+  </CFooter>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter'
+}
+</script>
